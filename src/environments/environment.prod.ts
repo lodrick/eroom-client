2754099fb1cecc382bb29a4ded4aae8f3c7a4b42
Lodrick Mpanze <lodrick1@dicovery.co.za>
2021-10-08 04:35:22 +0200

    well

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD_lhk8D69OGHxDrAAaXlibOc-FySGkeOs",
+    authDomain: "eroomapp-74e1f.firebaseapp.com",
+    projectId: "eroomapp-74e1f",
+    storageBucket: "eroomapp-74e1f.appspot.com",
+    messagingSenderId: "850176006314",
+    appId: "1:850176006314:web:d99de40b1dd1dd6f6b98d3",
+    measurementId: "G-7KWHKZ2QEF"
+  }
 };
